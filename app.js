2b9a44538b6d66fa9be662131b63ebdf4187f6e3
BryanU <ungbryan01@gmail.com>
2022-12-06 23:45:09 -0800

    Change port number

--- app.js
+++ app.js
@@ -662,4 +662,4 @@ io.on('connection', function(socket) {
 })
 
 
-server.listen(process.env.PORT || 5000, () => console.log("app listening on port 5000!"));
+server.listen(process.env.PORT || 3000, () => console.log("app listening on port 5000!"));
