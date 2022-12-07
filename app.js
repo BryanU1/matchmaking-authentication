3ffc7627bf32b871ea160a668933946aa44d340f
BryanU <ungbryan01@gmail.com>
2022-12-07 00:34:48 -0800

    Update app.js

--- app.js
+++ app.js
@@ -664,5 +664,4 @@ io.on('connection', function(socket) {
 
 server.listen(process.env.PORT || 3000, () => {
   console.log("app listening on port 3000!");
-  console.log(process.env.PASSWORD);
 });
