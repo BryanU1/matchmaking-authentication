ec65e9182527fbee542529f8b5f9cb8dea2e3fb7
BryanU <ungbryan01@gmail.com>
2022-12-07 17:24:41 -0800

    Change to port 80

--- app.js
+++ app.js
@@ -662,6 +662,6 @@ io.on('connection', function(socket) {
 })
 
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log("app listening on port 3000!");
+server.listen(80, () => {
+  console.log("app listening on port 80");
 });
